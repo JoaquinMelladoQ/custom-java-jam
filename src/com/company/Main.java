ba13e31cda2919bab26da7bfb6494648c32afba3
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-10 18:12:40 -0300

    added different types of variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello from java");
+        int number = 100;
+        double value = 100.02;
+        String brand = "Jamjam";
+        Date date = new Date();
     }
 }
