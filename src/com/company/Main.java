006a8ff0a6b46ca3df4adc195461a74af1930d6c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-10 22:31:19 -0300

    underscore was added to see numbers better

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         byte theByte = -128;
-        short theShort = 8989;
-        int theInt = 787838334;
-        long theLong = 78783833432L;
+        short theShort = 8_989;
+        int theInt = 787_838_334;
+        long theLong = 78_783_833_432L;
 
         float pi = 3.14F;
         double doublePi = 3.1415;
