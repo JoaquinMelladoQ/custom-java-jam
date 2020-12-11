a6b2071b28a42c7a0912cb45fc7918ee27c794f0
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-10 22:34:41 -0300

    reference types was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        byte theByte = -128;
-        short theShort = 8_989;
-        int theInt = 787_838_334;
-        long theLong = 78_783_833_432L;
+//        byte theByte = -128;
+//        short theShort = 8_989;
+//        int theInt = 787_838_334;
+//        long theLong = 78_783_833_432L;
+//
+//        float pi = 3.14F;
+//        double doublePi = 3.1415;
+//
+//        boolean isAdult = true;
+//        char nameInitial = 'A';
+//
+//        System.out.println(theByte);
+//        System.out.println(theShort);
+//        System.out.println(theInt);
+//        System.out.println(theLong);
+//        System.out.println(pi);
+//        System.out.println(doublePi);
+//        System.out.println(isAdult);
+//        System.out.println(nameInitial);
 
-        float pi = 3.14F;
-        double doublePi = 3.1415;
-
-        boolean isAdult = true;
-        char nameInitial = 'A';
-
-        System.out.println(theByte);
-        System.out.println(theShort);
-        System.out.println(theInt);
-        System.out.println(theLong);
-        System.out.println(pi);
-        System.out.println(doublePi);
-        System.out.println(isAdult);
-        System.out.println(nameInitial);
+        String name = new String(original: "Amigoscode");
 
     }
 }
